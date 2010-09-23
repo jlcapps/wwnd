cac8a40aa510200671783780d89b50a6fb0cfcea
Lee Capps <himself@leecapps.com>
2010-09-23 15:44:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wwnd::Application.config.secret_token = '2be0f3cb5d77b7377aa229afc04f28d23de0fbaaa1bc520d8c39eba3bf5749e87f63dbbce91318b37767440abda6c126e39cb4aeb905216f33f50fbb133e7c8a'
