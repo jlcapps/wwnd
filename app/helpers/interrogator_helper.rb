e15b09144879435d8e3e75ea08ef38713031f5bd
Lee Capps <himself@leecapps.com>
2010-09-30 13:50:10 -0400

    Add InterrogatorHelper##formatted_answer
    This turns double newlines into html breaks and turns all caps into
    italics.

--- app/helpers/interrogator_helper.rb
+++ app/helpers/interrogator_helper.rb
@@ -1,2 +1,8 @@
 module InterrogatorHelper
+  def formatted_answer(answer)
+    answer.gsub!("\n\n", "<br /<br />")
+    @matches = answer.scan(/([A-Z]{2,})/).flatten
+    @matches.each { |m| answer.sub!(m, "<i>#{m.downcase}</i>") }
+    answer
+  end
 end
