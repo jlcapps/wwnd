5fa909950dde1817e42131def9738f1aa0f71361
Lee Capps <himself@leecapps.com>
2010-10-07 09:44:05 -0400

    Italicize words set off w/ underscores

--- app/helpers/interrogator_helper.rb
+++ app/helpers/interrogator_helper.rb
@@ -2,7 +2,8 @@ module InterrogatorHelper
   def formatted_answer(answer)
     answer.gsub!("\n\n", "<br /<br />")
     @matches = answer.scan(/([A-Z]{2,})/).flatten
-    @matches.each { |m| answer.sub!(m, "<i>#{m.downcase}</i>") }
-    answer
+    @matches += answer.scan(/_.+?_/m).flatten
+    @matches.each { |m| answer.sub!(m, "<em>#{m.downcase}</em>") }
+    answer.gsub("_", "")
   end
 end
