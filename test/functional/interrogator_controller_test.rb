2e5127745fd4f9703dee09bdb5a817872221ae48
Lee Capps <himself@leecapps.com>
2010-10-11 13:23:13 -0400

    Add functional test with query

--- test/functional/interrogator_controller_test.rb
+++ test/functional/interrogator_controller_test.rb
@@ -1,8 +1,42 @@
 require 'test_helper'
 
 class InterrogatorControllerTest < ActionController::TestCase
+  def setup
+    # stub out Chunk#answer and Chunk##answer to route around problem with 
+    # Thinking Sphinx
+    def Chunk.answer(slug, question, chunk_id=nil)
+      @chunk = Chunk.new(:title => "My title", 
+                         :body => "what if truth were a woman?", 
+                         :work => Author.find_by_slug("nietzsche").works.first)
+
+      def @chunk.answer(snip="truth &#8230; woman")
+        Answer.new("what if truth were a woman", snip).to_s
+      end
+      [@chunk]
+    end
+  end
+
+  def teardown
+    load "app/models/chunk.rb"
+  end
+
   test "should get index" do
     get :index, :slug => "nietzsche"
+    assert_response :success
+  end
+
+# XXX - TS currently not working in tests. See comment in test_helper.rb
+#
+#   test "should get index with question" do
+#     ThinkingSphinx::Test.run do
+#       get :index, :slug => "nietzsche", :question => "what is mystring"
+#       assert_response :success
+#     end
+#   end
+
+  # This uses the stub in setup because of prolem w/ Thinking Sphinx in tests
+  test "should get index with question" do
+    get :index, :slug => "nietzsche", :question => "is truth a woman?"
     assert_response :success
   end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,13 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# XXX -- this should set up functional testing with TS, but using TS in tests
+# currently causes mysqld to soak up all the CPU when the Sphinx indexer is
+# running. So I will stub out the search until this can be resolved.
+#
+# require 'thinking_sphinx/test'
+# ThinkingSphinx::Test.init
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
