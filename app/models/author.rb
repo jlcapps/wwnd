acb0ec5835ef8d0daf2b170e56b1a99673937656
Lee Capps <himself@leecapps.com>
2010-10-15 15:54:18 -0400

    Add Author##active_and_in_order
    Moving a somewhat complicated search, used in two different controller
    methods, into the model for DRY.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,10 @@
 class Author < ActiveRecord::Base
   validates_presence_of :name, :display_name, :bio, :slug
   has_many :works, :order => "title"
+
+  scope :actives, where(:active => true).order("display_name")
+
+  def self.active_and_in_order
+    actives.group_by { |a| a.slug == "everybody" }.values.flatten
+  end
 end
--- test/unit/author_test.rb
+++ test/unit/author_test.rb
@@ -10,4 +10,8 @@ class AuthorTest < ActiveSupport::TestCase
     assert_equal 1, @author.works.count
     assert_equal "MyString", @author.works.first.title
   end
+
+  test "Author class should respond to active_and_in_order" do
+    assert_respond_to Author, :active_and_in_order
+  end
 end
