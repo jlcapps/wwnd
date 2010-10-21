b4826a065a07778c10eb775149a09d2cc52a53d2
Lee Capps <himself@leecapps.com>
2010-10-21 12:39:04 -0400

    Refactor for DRY, less state

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,18 +1,19 @@
 class Answer
 
-  attr_accessor :body, :snippet, :snip_pattern
+  attr_accessor :body, :snippet
   def initialize(body, snippet)
     @body, @snippet = body, snippet
 
     # build a match pattern based on Sphinx excerpts markup
-    snip_arr = @snippet.split("&#8230;").reject { |s| s.strip!.empty? }
-    first = Regexp.escape(snip_arr.first)
-    last =  Regexp.escape(snip_arr.last)
-    @snip_pattern = if first == last
-      /(.+[.?!]\)?["']?\s+)?(['"]?[A-Z]?.*#{first}[^.?!]*[.?!]?["']?)/m
-    else
-      /(.+[.?!]\)?["']?\s+)?(['"]?[A-Z]?.*#{first}.*#{last}[^.?!]*[.?!]?["']?)/m
-    end
+#     snip_arr = @snippet.split("&#8230;").reject { |s| s.strip!.empty? }
+#     first = Regexp.escape(snip_arr.first)
+#     last =  Regexp.escape(snip_arr.last)
+#     @snip_pattern = if first == last
+#       /(.+[.?!]\)?["']?\s+)?(['"]?[A-Z]?.*#{first}[^.?!]*[.?!]?["']?)/m
+#     else
+#       /(.+[.?!]\)?["']?\s+)?(['"]?[A-Z]?.*#{first}.*#{last}[^.?!]*[.?!]?["']?)/m
+#     end
+    @snip_pattern =   /(.+[.?!]\)?["']?\s+)?(['"]?[A-Z]?.*#{anchor}[^.?!]*[.?!]?["']?)/m
   end
 
   def to_s
@@ -26,5 +27,19 @@ class Answer
                  ActiveRecord::Base.logger.debug msg
                  @body
                end
+  end
+
+protected
+
+  def anchor
+    @snippet.split("&#8230;").reject { |s| s.strip!.empty? }.
+      values_at(0, -1).
+      uniq.
+      collect { |s| Regexp.escape(s) }.
+      join(".*")
+  end
+
+  def snip_pattern
+    /(.+[.?!]\)?["']?\s+)?(['"]?[A-Z]?.*#{anchor}[^.?!]*[.?!]?["']?)/m
   end
 end
