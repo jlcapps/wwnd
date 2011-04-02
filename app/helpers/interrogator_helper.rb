c5e354cfd6f03fbada967e9ba5928b513c7b7bc9
Lee Capps <himself@leecapps.com>
2011-04-02 17:11:34 -0400

    Fix tag

--- app/helpers/interrogator_helper.rb
+++ app/helpers/interrogator_helper.rb
@@ -1,6 +1,6 @@
 module InterrogatorHelper
   def formatted_answer(answer)
-    answer.gsub!("\n\n", "<br /<br />")
+    answer.gsub!("\n\n", "<br /><br />")
     @matches = answer.scan(/([A-Z]{2,})/).flatten
     @matches += answer.scan(/_.+?_/m).flatten
     @matches.each { |m| answer.sub!(m, "<em>#{m.downcase}</em>") }
